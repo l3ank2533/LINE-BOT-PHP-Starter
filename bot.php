0b11139e207d3a8a07233bc501430da586a6a390
l3ank2533 <ballboad@hotmail.com>
2017-06-21 11:52:00 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $str
+				'text' => strpos($text,"สวัสดี")
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
